9e11210449e85e1d8d59d82ac3384050e985285e
Edanur12 <b210109052@subu.edu.tr>
2022-11-08 21:31:51 +0300

    added home app

---
+++ home/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+# Create your views here.
+def index(request):
+    text = "merhaba dunya"
+    # return HttpResponse(text)
+    context = {'text2': text}
+    return render(request, 'index.html', context)
