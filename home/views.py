98c538978393c58689557f2d2c8724ed4ffd96d5
Edanur12 <b210109052@subu.edu.tr>
2022-11-16 18:16:41 +0300

    product olusturma

--- home/views.py
+++ home/views.py
@@ -5,6 +5,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     text = "merhaba dunya"
-    # return HttpResponse(text)
+    return HttpResponse(text)
     context = {'text2': text}
     return render(request, 'index.html', context)
--- product/urls.py
+++ product/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = {
-     path('', include('home.urls')),
-    path('home/', include('home.urls')),
-    path('admin/', admin.site.urls),
-    path('product/', include('product.urls')),
+from home import views
 
-}
+urlpatterns = [
+
+    path('', views.index, name='index'),
+
+]
