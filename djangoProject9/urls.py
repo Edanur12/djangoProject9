b920bbaa123abf1579fc852375ad60931b8ea79c
Edanur12 <b210109052@subu.edu.tr>
2022-12-09 00:57:14 +0300

    category_products.html

--- djangoProject9/urls.py
+++ djangoProject9/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('home/', include('home.urls')),
     path('admin/', admin.site.urls),
     path('product/', include('product.urls')),
-    path('category/<int:id>/<slug:slug>', views.category_products, name='category_products'),]
+    path('Category/<int:id>/<slug:slug>', views.category_products, name='category_products'),]
 if settings.DEBUG:  # new
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
